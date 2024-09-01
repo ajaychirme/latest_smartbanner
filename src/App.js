727e19d5f3b75fd8e2272891d6066607ad0beb02
ajaychirme <ajaychirme84@gmail.com>
2024-09-01 19:49:16 +0530

    market url

--- src/App.js
+++ src/App.js
@@ -5,19 +5,23 @@ function App() {
 
   const handleOpen = () => {
     if (/android/i.test(navigator.userAgent)) {
-      // window.location.href = "intent://amazon.com/#Intent;scheme=https;package=com.amazon.mShop.android.shopping;end";
-      // window.location.href = "intent://amazon.in/#Intent;scheme=https;package=com.amazon.mShop.android.shopping;end";
-      //window.location.href = "intent://com.amazon.mShop.android.shopping/#Intent;scheme=https;package=com.amazon.mShop.android.shopping;end";
-      window.location.href = "market://details?id=com.amazon.mShop.android.shopping";
+      // Attempt to open the Amazon app
+      window.location.href = "intent://com.amazon.mShop.android.shopping/#Intent;scheme=https;package=com.amazon.mShop.android.shopping;end";
+  
+      // Fallback to Play Store if the app isn't opened
+      setTimeout(() => {
+        window.location.href = "market://details?id=com.amazon.mShop.android.shopping";
+      }, 2000); // Delay before fallback
     } else {
-      // Fallback or message for non-Android users
-      alert("This action is only supported on Android devices.");
+      // Redirect to Amazon India website for non-Android users
+      window.location.href = "https://www.amazon.in";
     }
   };
+  
 
   return (
     <div className="App">
-      <p>Market</p>
+      <p>Market 1</p>
       <img
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTy8L1HIH2ZlhTcSR2x5c993GIA6DFFs06YEg&s"
         alt=""
