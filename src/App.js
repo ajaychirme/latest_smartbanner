ec5e69af30ef25aae238ece9a8931805ffbf524f
ajaychirme <ajaychirme84@gmail.com>
2024-09-02 16:04:07 +0530

    new12

--- src/App.js
+++ src/App.js
@@ -1,32 +1,60 @@
-import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  // const [applist, setApplist] = useState([]);
 
   const handleOpen = () => {
-    const amazonUri = 'intent://scan/#Intent;scheme=nxtr;package=com.totum.student;end;';
-    const fallbackUrl = 'https://www.amazon.com/dp/B08J5F3G18';
+    // if (/android/i.test(navigator.userAgent)) {
+    //   // Attempt to open the Amazon app
+    //   window.location.href = "intent://com.amazon.mShop.android.shopping/#Intent;scheme=https;package=com.amazon.mShop.android.shopping;end";
+  
+    //   // Fallback to Play Store if the app isn't opened
+    //   setTimeout(() => {
+    //     window.location.href = "market://details?id=com.amazon.mShop.android.shopping";
+    //   }, 2000); // Delay before fallback
+    // } else {
+    //   // Redirect to Amazon India website for non-Android users
+    //   window.location.href = "https://www.amazon.in";
+    // }
 
+    // const amazonUri = 'nxtr://nux.new.user.joinus';
+
+    const amazonUri='intent://scan/#Intent;scheme=nxtr;package=com.totum.student;end;'
+
+    // Define the fallback URL (e.g., a specific product page on Amazon)
+    const fallbackUrl = 'https://www.amazon.com/dp/B08J5F3G18'; // Replace with your product URL or relevant Amazon page
+
+    // Attempt to open the Amazon app using the URI scheme
+    // window.location.href = amazonUri;
     window.location.replace(amazonUri);
     clearHistory();
+    // Redirect to fallback URL after a short delay if the app is not installed
+    // setTimeout(() => {
+    //   alert('set timeout...')
+    //   window.location.href = fallbackUrl;
+    // }, 1000); // Adjust the timeout duration if necessary
   };
 
   function clearHistory() {
+    // Push an empty state to clear URL fragment
     window.history.pushState({}, '', '/');
+    // Push another state to ensure URL is reset
     window.history.pushState({}, '', '/');
+    // Remove the current state from history
     window.history.go(-1);
   }
 
   return (
     <div className="App">
-      <p>Market android check nxtr added revert</p>
+      <p>Market android check nxtr added</p>
+      <p>Applist is === {applist}</p>
       <img
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTy8L1HIH2ZlhTcSR2x5c993GIA6DFFs06YEg&s"
-        alt="App Logo"
+        alt=""
       />
       <div className="button-container">
+        {/* <button className="btn" onClick={handleInstall}>Install the app</button> */}
         <button className="btn2" onClick={handleOpen}>
           Open the app
         </button>
