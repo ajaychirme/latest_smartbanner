fe7db8fc3c6e6294d0e11388d778f2d9b06cc6fe
ajaychirme <ajaychirme84@gmail.com>
2024-12-06 11:35:45 +0530

    04dec-24

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [buttonText, setButtonText] = useState("");
 
   const handleOpen = () => {
-    const appUrl = "nxtr://";
+    const appUrl = "nxtr://nux.user.search";
     const playStoreUrl = "intent://details?id=com.totum.student#Intent;scheme=market;package=com.android.vending;end;";
     const timeout = 500; // Time in milliseconds before redirecting to Play Store
     let hasAppOpened = false;
