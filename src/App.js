92a1ef01396cc1b179586b51f955cf45d2b78a87
ajaychirme <ajaychirme84@gmail.com>
2024-12-08 18:59:00 +0530

    04dec35

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import { Plugins } from '@capacitor/core';
-import { App } from '@capacitor/app';
+import React, { useEffect, useState } from "react";
+import { Plugins } from "@capacitor/core";
+import { App } from "@capacitor/app";
 
 const AppCheck = () => {
   const [isInstalled, setIsInstalled] = useState(null); // `null` for initial loading state
-
+  const [error, setError] = useState(null);
+  const [result, setResult] = useState(null);
   useEffect(() => {
     const checkAppInstalled = async (packageName) => {
       try {
         const result = await App.isAppInstalled({ packageName });
+        setResult(result);
         setIsInstalled(result.installed); // Set the state based on the result
       } catch (error) {
-        console.error('Error checking app installation:', error);
+        console.error("Error checking app installation:", error);
+        setError(error);
         setIsInstalled(false); // Default to `false` if there's an error
       }
     };
 
     // Call the function with the desired package name
-    checkAppInstalled('com.totum.student');
+    checkAppInstalled("com.totum.student");
   }, []);
 
   return (
@@ -31,6 +34,8 @@ const AppCheck = () => {
       ) : (
         <p>The app is not installed.</p>
       )}
+      {error ? <h4>{error}</h4> : "No error"}
+      {result ? <h4>{result}</h4> : "No result"}
     </div>
   );
 };
