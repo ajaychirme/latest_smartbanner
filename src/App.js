0b7bf4b9805c23d0915651167ca0ac0ccb9fe554
ajaychirme <ajaychirme84@gmail.com>
2024-12-08 18:39:33 +0530

    04dec33

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Plugins } from '@capacitor/core';
 import { App } from '@capacitor/app';
 
-const app = () => {
+const App = () => {
   const [isInstalled, setIsInstalled] = useState(null); // `null` for initial loading state
 
   useEffect(() => {
@@ -35,4 +35,4 @@ const app = () => {
   );
 };
 
-export default app;
+export default App;
