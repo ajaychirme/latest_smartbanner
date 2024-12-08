6aa5005142aaa734702256962fa97ed39e164357
ajaychirme <ajaychirme84@gmail.com>
2024-12-08 21:06:56 +0530

    04dec39

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import React, { useEffect, useState } from "react";
 
 const AppCheck = () => {
   const [isAppInstalled, setIsAppInstalled] = useState(false);
-
+  const [result, setResult] = useState([]);
+  const [error, setError] = useState();
   useEffect(() => {
     if ("getInstalledRelatedApps" in navigator) {
       navigator
         .getInstalledRelatedApps()
         .then((relatedApps) => {
+          setResult(relatedApps)
           const appInstalled = relatedApps.some(
             (app) => app.id === "com.totum.student"
           );
           setIsAppInstalled(appInstalled);
         })
         .catch((error) => {
+          setError(error)
           console.log("Hiii");
           console.error("Error checking related apps:", error);
         });
@@ -27,6 +30,8 @@ const AppCheck = () => {
   return (
     <div>
       <h1>Check if App is Installed</h1>
+      {result?(<h3>{result}</h3>):<h3>No result</h3>}
+      {error?(<h3>{error}</h3>):<h3>No result</h3>}
       {isAppInstalled ? (
         <p>The Totum native app is installed!</p>
       ) : (
