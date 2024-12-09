8d0b6b5c732f76a94e1ae82aa726e041c90351ad
ajaychirme <ajaychirme84@gmail.com>
2024-12-09 15:31:03 +0530

    04dec45

--- src/App.js
+++ src/App.js
@@ -6,28 +6,33 @@ function App() {
   const [buttonText, setButtonText] = useState("");
 
   const handleOpen = () => {
-    const appUrl = "nxtr://nux.user.search";
-    const playStoreUrl =
-      "intent://details?id=com.totum.student#Intent;scheme=market;package=com.android.vending;end;";
-    const timeout = 500; // Time in milliseconds before redirecting to Play Store
+    const currentUrl = 'https://develop.totum.com/discount/ambassador-snooker-and-pool-clubs/get_offer_totum_app';
 
+    const appUrl = currentUrl.includes('/discount/')
+      ? `nxtr://open?url=${encodeURIComponent(currentUrl)}`
+      : 'nxtr://nux.user.search';
+    
+    const playStoreUrl = 'intent://details?id=com.totum.student#Intent;scheme=market;package=com.android.vending;end;';
+    const timeout = 500; // Time in milliseconds before redirecting to Play Store
+    let hasAppOpened = false;
+    
     // Open the app using its custom URL scheme
     window.location.href = appUrl;
-
+    
     // Use a timeout to detect if the app was not installed
     setTimeout(() => {
       if (!hasAppOpened) {
-        console.log("App not installed, redirecting to Play Store...");
-        window.location.replace(playStoreUrl, "_blank");
+        console.log('App not installed, redirecting to Play Store...');
+        window.location.replace(playStoreUrl);
       }
     }, timeout);
-
+    
     // Add an event to confirm the user has the app installed (optional)
-    document.addEventListener("visibilitychange", () => {
-      if (document.visibilityState === "hidden") {
-        setHasAppOpened(true);
+    document.addEventListener('visibilitychange', () => {
+      if (document.visibilityState === 'hidden') {
+        hasAppOpened = true;
       }
-    });
+    });    
   };
 
   useEffect(() => {
